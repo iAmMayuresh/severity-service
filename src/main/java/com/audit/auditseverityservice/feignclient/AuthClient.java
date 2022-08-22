4c759ecad4decad78a561bcdbe71486cb0178d6c
elisarelhan <79890999+elisarelhan@users.noreply.github.com>
2022-08-22 14:06:53 +0530

    Add files via upload

--- src/main/java/com/audit/auditseverityservice/feignclient/AuthClient.java
+++ src/main/java/com/audit/auditseverityservice/feignclient/AuthClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "auth-service", url = "$(AUTH_SERVICE_URI:http://localhost:8000)")
+@FeignClient(name = "auth-service", url = "${AUTH_SERVICE_URI:http://localhost:8000}")
 //@FeignClient(name = "auth-service", url = "http://localhost:8000")
 public interface AuthClient {
 
